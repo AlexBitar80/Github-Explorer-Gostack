59582ba4622b75cf5560cd1d768a6649a064f665
Alexandre Bitar <joabitar76@gmail.com>
2020-11-27 13:48:39 -0300

    feat: added main component of the dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,63 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>;
+import logoImg from '../../assets/Logo.svg';
+
+import { Title, Form, Repositories } from './styles';
+
+const Dashboard: React.FC = () => {
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github.</Title>
+
+      <Form>
+        <input placeholder="Digite aqui" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/56983783?s=460&u=8f9989cae85f76bc242ece4414271ccc54aeba56&v=4"
+            alt="Alexandre"
+          />
+          <div>
+            <strong>AlexBitar80/Fundamentos-NodeJS</strong>
+            <p>desafio do bootcamp gostack fundamentos Node.js</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/56983783?s=460&u=8f9989cae85f76bc242ece4414271ccc54aeba56&v=4"
+            alt="Alexandre"
+          />
+          <div>
+            <strong>AlexBitar80/Fundamentos-NodeJS</strong>
+            <p>desafio do bootcamp gostack fundamentos Node.js</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/56983783?s=460&u=8f9989cae85f76bc242ece4414271ccc54aeba56&v=4"
+            alt="Alexandre"
+          />
+          <div>
+            <strong>AlexBitar80/Fundamentos-NodeJS</strong>
+            <p>desafio do bootcamp gostack fundamentos Node.js</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
+};
 
 export default Dashboard;
