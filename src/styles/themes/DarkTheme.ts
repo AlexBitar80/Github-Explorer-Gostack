d63d8505364e1729766af14d20382ff1903d2525
Alexandre Bitar <joabitar76@gmail.com>
2021-03-04 19:01:07 -0300

    chore: added dark and light themes

---
+++ src/styles/themes/DarkTheme.ts
@@ -0,0 +1,10 @@
+export default {
+  title: 'dark',
+
+  colors: {
+    box: '#6272a4',
+    background: '#282a36',
+    text: '#fff',
+    title: '#f8f8f2',
+  },
+};
---
+++ src/styles/themes/LightTheme.ts
@@ -0,0 +1,10 @@
+export default {
+  title: 'light',
+
+  colors: {
+    box: '#fff',
+    background: '#f0f0f5',
+    text: '#a8a8b3',
+    title: '#3a3a3a',
+  },
+};
